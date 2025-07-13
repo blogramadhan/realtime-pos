37a474d01a7e270137d78149b981679a147724d7
Kurnia Ramadhan <96585178+blogramadhan@users.noreply.github.com>
2025-07-13 15:00:45 +0700

    Menambahkan konfigurasi ESLint baru dan memperbarui dependensi di package.json serta package-lock.json. Juga memperbarui file globals.css untuk mendukung tema dan animasi baru.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,13 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+eslintConfig.push({
+  rules: {
+    "react-hooks/exhaustive-deps": "off",
+    "react/jsx-key": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+});
+
 export default eslintConfig;
